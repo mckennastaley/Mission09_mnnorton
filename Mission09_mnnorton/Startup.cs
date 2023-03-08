683b0ea86d581852975dfb8e523fdc38def3a715
McKenna Staley <mnnorton@byu.edu>
2023-03-08 16:50:29 -0700

    Button for purchase :)

--- Mission09_mnnorton/Startup.cs
+++ Mission09_mnnorton/Startup.cs
@@ -33,6 +33,8 @@ namespace Mission09_mnnorton
            });
 
             services.AddScoped<IBookstoreRepository, EFBookstoreRepository>();
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,6 +66,8 @@ namespace Mission09_mnnorton
                     "type", "{bookType}", new { Controller = "Home", action = "Index", pageNum = 1 });
 
                 endpoints.MapDefaultControllerRoute();
+
+                endpoints.MapRazorPages();
             });
         }
     }
