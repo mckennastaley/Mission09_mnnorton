934abb61b4c8695c7e72c85064747a2e4519d662
McKenna Staley <mnnorton@byu.edu>
2023-03-07 17:43:01 -0700

    finish CSS

--- Mission09_mnnorton/Infrastructure/PagenationTagHelpercs.cs
+++ Mission09_mnnorton/Infrastructure/PagenationTagHelpercs.cs
@@ -31,6 +31,10 @@ namespace Mission09_mnnorton.Infrastructure
         //different from view context:
         public PageInfo PageBlah { get; set; }
         public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
 
 
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
@@ -44,6 +48,15 @@ namespace Mission09_mnnorton.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
+
+                tb.AddCssClass(PageClass);
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
