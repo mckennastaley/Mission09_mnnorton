fc3f0078d3719e68e6fe2e040892b2bb8594765b
McKenna Staley <mnnorton@byu.edu>
2023-03-08 17:41:06 -0700

    OOOOOOOOOHHHH we're halfway theERE ooohOOH living on a prayER

---
+++ Mission09_mnnorton/Models/Basket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_mnnorton.Models
+{
+    public class Basket
+    {
+        public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
+
+        public void AddItem(Book proj, int qty)
+        {
+            BasketLineItem Line = Items
+                .Where(p => p.Book.BookId == proj.BookId)
+                .FirstOrDefault();
+
+            if (Line == null)
+            {
+                Items.Add(new BasketLineItem
+                {
+                    Book = proj,
+                    Quantity = qty
+                });
+            }
+            else
+            {
+                Line.Quantity += qty;
+            }
+        }
+
+        public double CalculateTotal()
+        {
+            double sum = Items.Sum(x => x.Quantity * x.Book.Price);
+            return sum;
+        }
+    }
+
+
+
+    public class BasketLineItem
+    {
+        public int LineID { get; set; }
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- Mission09_mnnorton/Pages/Purchase.cshtml.cs
+++ Mission09_mnnorton/Pages/Purchase.cshtml.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission09_mnnorton.Models;
 
 namespace Mission09_mnnorton.Pages
 {
     public class PurchaseModel : PageModel
     {
-        public void OnGet()
+        private IBookstoreRepository repo { get; set; }
+
+        public PurchaseModel(IBookstoreRepository temp)
         {
+            repo = temp;
+        }
+
+        public Basket basket { get; set; }
+
+        public void OnGet(Basket b)
+        {
+            basket = b;
+        }
+
+        public IActionResult OnPost(int BookId)
+        {
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == BookId);
+
+            basket = new Basket();
+
+            basket.AddItem(b, 1);
+
+            return RedirectToPage(basket);
         }
     }
 }
