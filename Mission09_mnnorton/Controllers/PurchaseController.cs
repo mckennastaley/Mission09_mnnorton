039d58178315ad3c08047db24e28d89f5a2dbed8
McKenna Staley <mnnorton@byu.edu>
2023-03-15 16:01:06 -0600

    Grateful this works

---
+++ Mission09_mnnorton/Controllers/PurchaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_mnnorton.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_mnnorton.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View(new Purchase());
+        }
+    }
+}
