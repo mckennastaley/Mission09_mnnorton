07ddcb9de1fca5199bf5ec333d3375834fdd40fa
McKenna Staley <mnnorton@byu.edu>
2023-03-15 20:22:58 -0600

    Got the cart going yahyah

---
+++ Mission09_mnnorton/Components/BasketSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission09_mnnorton.Models;
+
+namespace Mission09_mnnorton.Components
+{
+    public class BasketSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public BasketSummaryViewComponent(Basket b)
+        {
+            basket = b;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+
+    }
+}
